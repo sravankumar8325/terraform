f0a73b493e5e3fd0ad5e2bb8ad560d4d9a342291
sravankumar8325 <94476911+sravankumar8325@users.noreply.github.com>
2021-11-22 21:23:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,33 @@
-resource "aws_quicksight_data_source" "quicksight_ds" {
-  data_source_id = "example-id"
-  name           = "quicksight_s3"
-
-  parameters {
-    s3 {
-      manifest_file_location {
-        bucket = "bucketname"
-        key    = "path/to/manifest.json"
-      }
-    }
-  }
-
-  type = "S3"
+resource "aws_iam_policy" "s3-full-access" {
+     name   = "S3FullAccess"
+     path   = "/"
+     policy = jsonencode(
+          {
+             Statement = [
+                 {
+                     Action   = "s3:*"
+                     Effect   = "Allow"
+                     Resource = "*"
+                     Sid      = "S3FullAccess"
+                  },
+              ]
+             Version   = "2012-10-17"
+          }
+    )
 }
+
+#resource "aws_quicksight_data_source" "quicksight_ds" {
+#  data_source_id = "example-id"
+#  name           = "quicksight_s3"
+#
+#  parameters {
+#    s3 {
+#      manifest_file_location {
+#        bucket = "bucketname"
+#        key    = "path/to/manifest.json"
+#      }
+#    }
+#  }
+#
+#  type = "S3"
+#}
