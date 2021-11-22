3a8c20320b48065f830f3d57fd289d55cad046cf
sravankumar8325 <94476911+sravankumar8325@users.noreply.github.com>
2021-11-22 21:49:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_iam_policy" "s3-full-access" {
 }
 
 resource "aws_iam_role" "my-iam-role" {
-     name = "iam-role"     
+     name = "my-iam-role"     
      assume_role_policy = "${file("assumerolepolicy.json")}"
+}
+
+resource "aws_iam_policy_attachment" "policy-attach" {
+  name       = "policy-attachment"
+  roles      = ["${aws_iam_role.my-iam-role.name}"]
+  policy_arn = "${aws_iam_policy.s3-full-access.arn}"
 }
 
 #resource "aws_quicksight_data_source" "quicksight_ds" {
